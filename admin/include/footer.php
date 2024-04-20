eb5a15eb5478c17271c874fe32c5db62b6815e8d
Jeffrey Jones S <135623574+JeffreyJones2003@users.noreply.github.com>
2024-04-20 12:45:38 +0530

    Add files via upload

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2023 Online Shopping Portal </b> | |  Developed by Varshini - Ahash - Ajith Kumar (Sindhi 3rd B.Sc CS)
+		</div>
+	</div>
